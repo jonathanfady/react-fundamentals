8eed4c3b93b791bc566a6bcbc70688eda6efce47
jonathanfady <jonathan.fady@gmail.com>
2022-10-21 10:36:13 +0200

    Simplify EditTodo, use Ref instead of State.

--- src/Todos/EditTodo.js
+++ src/Todos/EditTodo.js
@@ -1,36 +1,29 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef } from 'react';
 
 export default function EditTodo({ todo, dispatch }) {
-    const [name, setName] = useState(todo.name);
-    const [description, setDescription] = useState(todo.description);
-    const [status, setStatus] = useState(todo.status);
+    const name = useRef(todo.name)
+    const description = useRef(todo.description)
+    const status = useRef(todo.status)
 
     useEffect(() => {
-        setName(todo.name);
-        setDescription(todo.description);
-        setStatus(todo.status);
+        name.current.value = todo.name;
+        description.current.value = todo.description;
+        status.current.value = todo.status;
     }, [todo]);
 
-    function handleNameChange(e) {
-        setName(e.target.value);
-    }
-
-    function handleDescriptionChange(e) {
-        setDescription(e.target.value);
-    }
-
-    function handleStatusChange(e) {
-        setStatus(e.target.value);
-    }
-
     function handleClick() {
-        if ((name === "") || (description === "")) {
+        if ((name.current.value === "") || (description.current.value === "")) {
             return false;
         }
 
         dispatch({
             type: 'edit-todo',
-            payload: { id: todo.id, name, description, status }
+            payload: {
+                id: todo.id,
+                name: name.current.value,
+                description: description.current.value,
+                status: status.current.value
+            }
         });
     }
 
@@ -45,23 +38,20 @@ export default function EditTodo({ todo, dispatch }) {
                     <div className="modal-body">
                         <div className="col-4 form-floating mb-3">
                             <input type="text" id="editTodoName"
-                                value={name}
-                                onChange={handleNameChange}
+                                ref={name}
                                 className="form-control" placeholder="Todo Name" />
                             <label htmlFor="editTodoName">Name</label>
                         </div>
                         <div className="col-10 form-floating mb-3">
                             <textarea id="editTodoDescription"
-                                value={description}
-                                onChange={handleDescriptionChange}
+                                ref={description}
                                 className="form-control" placeholder="Todo Description"
                                 style={{ minHeight: '7rem' }}></textarea>
                             <label htmlFor="editTodoDescription">Description</label>
                         </div>
                         <div className="col-5 form-floating">
                             <select id="editTodoStatus"
-                                value={status}
-                                onChange={handleStatusChange}
+                                ref={status}
                                 className="form-select pb-1" aria-label="Todo Status">
                                 <option value="to-do">To Do</option>
                                 <option value="in-progress">In Progress</option>
