68b39790dc38370475bea63166f8d6602b72e4a3
jonathanfady <jonathan.fady@gmail.com>
2022-11-10 00:13:49 +0100

    Fix the Home page display.

--- src/Home.tsx
+++ src/Home.tsx
@@ -6,25 +6,20 @@ function Home() {
     const [count, setCount] = useState(0)
 
     return (
-        <div className="App">
+        <div className="text-center">
             <div>
-                <a href="https://vitejs.dev" target="_blank">
+                <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
                     <img src="/vite.svg" className="logo" alt="Vite logo" />
                 </a>
-                <a href="https://reactjs.org" target="_blank">
+                <a href="https://reactjs.org" target="_blank" rel="noreferrer">
                     <img src={reactLogo} className="logo react" alt="React logo" />
                 </a>
             </div>
             <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
+            <button className="m-2 btn btn-primary" onClick={() => setCount((count) => count + 1)}>
+                count is {count}
+            </button>
+            <p className="p-4 h5">
                 Click on the Vite and React logos to learn more
             </p>
         </div>
